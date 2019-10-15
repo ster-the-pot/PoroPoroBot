a7f5075a3837fe864a92305e8475c421e8f2bc6c
Sterling Trafford <Sterling_Trafford1@baylor.edu>
2019-10-14 22:20:49 -0500

    SCT command map implemented

--- src/commands/binfo.js
+++ src/commands/binfo.js
@@ -1,23 +1,36 @@
 const Discord = require("discord.js");
 const token = require("../../assets/token.json");
+const CommandInterface = require('../CommandInterface.js');
 
- 
 
-const binfo = (client, msg) => {
-        
-    let tMem = 0;
-    client.guilds.forEach(e=>{
-            tMem+=e.memberCount;
-    });    
-    info = new Discord.RichEmbed();
-    info.setThumbnail('https://pm1.narvii.com/5763/0bc3572cedb7ef976953855aeaf492a8b2b82a67_hq.jpg');        info.setURL('https://github.com/ster-the-pot/llfgProject');
-    info.setColor('#3273a8');
-    info.title='Poro Poro Bot!'
-    info.addField("Info:",token.info);
-    info.addField('Author:','Steelo!');
-    info.addField('Active Members:',tMem);
-
-    msg.channel.send(info);
+class binfo {
+    
+    static info(){
+        return ["binfo","usage: p/binfo","provides basic bot information to the user"];
     }
+
+      run(client, msg, args) {
+        let tMem = 0;
+        client.guilds.forEach(e => {
+            tMem += e.memberCount;
+        });
+        let infoE = new Discord.RichEmbed();
+        infoE.setThumbnail('https://pm1.narvii.com/5763/0bc3572cedb7ef9769538ß55aeaf492a8b2b82a67_hq.jpg');
+        infoE.setURL('https://github.com/ster-the-pot/llfgProject');
+        infoE.setColor('#3273a8');
+        infoE.title = 'Poro Poro Bot!'
+        infoE.addField("Info:", token.info);
+        infoE.addField('Author:', 'Steelo!');
+        infoE.addField('Active Members:', tMem);
+    
+        msg.channel.send(infoE).then((msg) => {
+            setTimeout
+            msg.delete(10000).catch(err => console.log("Error: Invalid Permissions to Delete"));
+
+        });
+
+    }
+
+}
 
 module.exports.binfo = binfo;
--- src/main.js
+++ src/main.js
@@ -8,9 +8,20 @@ const path = require("path");
 let cmdMap = new Map();
 
 
+const helpCommand = (msg) => {
+    help = new Discord.RichEmbed();
+    help.setThumbnail('https://pm1.narvii.com/5763/0bc3572cedb7ef976953855aeaf492a8b2b82a67_hq.jpg');
+    help.setTitle('Poro Poro Commands');
+    cmdMap.forEach((command, k) => {
+        i = command[k].info();
+        help.addField(i[0] + " " +  i[1], i[2]);     
+    });
+    return help;
+}
+
 const activity = ["against a fed Zed", "arguably better that Faker","only Riven","like a Wood Division IV","without Flash"];
 
-let playingReset = () => {
+const playingReset = () => {
     client.user.setActivity(activity[Math.floor(Math.random() * activity.length)],'PLAYING')
     .catch(()=>{
         console.log("Failed to set Bot Activity");
@@ -36,21 +47,31 @@ let cmdReadr = async() => {
         });
 
     });
-    console.log(cmdMap.size);
     
 };
 
 
 //all Commands handler
-let commandHandler = (msg) => {
+let   commandHandler = (msg) => {
     if(msg.content.substr(0,2) == token.prefix && !msg.author.bot){
         let args = msg.content.substr(2,msg.content.length).split(" ");
         let cmd = args.shift();
         if(cmdMap.has(cmd)){
-            cmdMap.get(cmd).binfo(client, msg);
-        }
+           command = cmdMap.get(cmd);
+           exec = new command[cmd]();
+        exec.run(client,msg,args);
+           
+           
+        }else(
+            msg.reply(" Command not found. Run p/help to get a list of commands!").catch(e=>console.log("Reply Failed")).then((m)=>{
+                m.delete(10000).catch(e=>console.log(e));
+            })
+        )
+
+       msg.delete().catch(e=>console.log("Invalid Permissions."));
+
        
-    }
+    };
 }
 
  client.on('ready', async ()=>{
