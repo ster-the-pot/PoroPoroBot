ddf3f8443751ad1d1f3cff0dbb3b0457c16ae231
Sterling Trafford <Sterling_Trafford1@baylor.edu>
2019-10-17 17:51:54 -0500

    SUMM: getSummonerPart

--- src/commands/summoner.js
+++ src/commands/summoner.js
@@ -15,42 +15,79 @@ class Summoner {
         this.args = args;
     }
 
-    async getSummoner(sum) {
+    async getSummonerID(sum) {
         try {
             let response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${sum}`, {
                 params: {
                     api_key: token.lToken
                 }
-            }).catch(()=>console.log("error"));
-
-
+            })
+            let r2 = await axios.get(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${response.data.id}`, {
+                params: {
+                    api_key: token.lToken
+                }
+            })
             let embed = new Discord.RichEmbed();
             embed.setThumbnail(`http://ddragon.leagueoflegends.com/cdn/9.20.1/img/profileicon/${response.data.profileIconId}.png`)
             embed.setTitle(response.data.name);
             embed.addField("Summoner Level:", response.data.summonerLevel);
+            console.log(r2.data);
+            embed.addField("Summoner Rank:",r2.data.rank);
+            embed.addField("Summoner Tier:",r2.data.tier);
             this.msg.channel.send(embed);
 
-
-        } catch (err) {
-            if(err.status=='404'){
-                this.msg.reply("Sorry! We couldn't find that summoner. Please check your spelling and try again!");
-            }
-            console.log(err);
-
+    }catch(err) {
+        if (err.status == '404') {
+            this.msg.reply(" I couldn't find the summoner you were looking for! Check for typo's and try again!").then((m) => {
+                m.delete(6000).catch(err => console.log("Failed Reply Deletion"));
+            });
         }
+        console.log(err);
     }
 
 
-    async run() {
-        if (this.args.length != 2) {
-            this.msg.reply('usage: ' + Summoner.command_info()['usage']).then((m) => m.delete(7000).catch(e => console.log(e)));
-        }
-        if (this.args[1] == 'set') {
-            console.log("Setting Summoner Name is currently unavailable!");
+}
 
-        } else if (this.args[0] == 'get') {
-            await this.getSummoner(this.args[1]);
-        }
+
+async run() {
+    if (this.args.length != 2) {
+        this.msg.reply('usage: ' + Summoner.command_info()['usage']).then((m) => m.delete(7000).catch(e => console.log(e)));
+    }
+    if (this.args[1] == 'set') {
+        console.log("Setting Summoner Name is currently unavailable!");
+
+    } else if (this.args[0] == 'get') {
+        await this.getSummonerID(this.args[1]);
+    }
+}
+}
+
+class SummonerData {
+    constructor(eId, name, pIcon, rank) {
+        this.name = name;
+        this.eId = eId;
+        this.pIcon = pIcon;
+
+    }
+    setRank() {
+        this.rank = rank;
+    }
+    getRank() {
+        return this.rank;
+    }
+    setPIcon(p) {
+        this.pIcon = p;
+    }
+    getPIcon() {
+        return this.pIcon;
+    }
+}
+
+class Rank {
+    constructor(level, rank, tier) {
+        this.tier = tier;
+        this.rank = rank;
+        this.level = level;
     }
 }
 
