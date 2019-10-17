7f8da4c03136243c92df01ba687cf0979d8f9f9b
Sterling Trafford <Sterling_Trafford1@baylor.edu>
2019-10-16 22:01:52 -0500

    CMD: summoner axios, need to do error checking

--- src/commands/summoner.js
+++ src/commands/summoner.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
-
+const axios = require("axios");
+const token = require('../../assets/token.json');
 class Summoner {
     static command_info() {
         return {
@@ -14,17 +15,43 @@ class Summoner {
         this.args = args;
     }
 
-    async run(){
-        if(this.args.length!=2){
-            console.log(this.args.length);
-            this.msg.reply('usage: ' + Summoner.command_info()['usage']).then((m)=>m.delete(7000).catch(e=>console.log(e)));
+    async getSummoner(sum) {
+        try {
+            let response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${sum}`, {
+                params: {
+                    api_key: token.lToken
+                }
+            }).catch(()=>console.log("error"));
+
+
+            let embed = new Discord.RichEmbed();
+            embed.setThumbnail(`http://ddragon.leagueoflegends.com/cdn/9.20.1/img/profileicon/${response.data.profileIconId}.png`)
+            embed.setTitle(response.data.name);
+            embed.addField("Summoner Level:", response.data.summonerLevel);
+            this.msg.channel.send(embed);
+
+
+        } catch (err) {
+            if(err.status=='404'){
+                this.msg.reply("Sorry! We couldn't find that summoner. Please check your spelling and try again!");
+            }
+            console.log(err);
+
         }
-        if(this.args[1] == 'set'){
+    }
 
-        }else if(this.args[1] == 'get'){
 
+    async run() {
+        if (this.args.length != 2) {
+            this.msg.reply('usage: ' + Summoner.command_info()['usage']).then((m) => m.delete(7000).catch(e => console.log(e)));
+        }
+        if (this.args[1] == 'set') {
+            console.log("Setting Summoner Name is currently unavailable!");
+
+        } else if (this.args[0] == 'get') {
+            await this.getSummoner(this.args[1]);
         }
     }
 }
 
-module.exports.summoner=Summoner;
+module.exports.summoner = Summoner;
